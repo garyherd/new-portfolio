038640fb4dc6341d99579649bfa5a213eb6b363e
Gary Herd <garyherd5@gmail.com>
2017-07-19 20:29:07 -0500

    Add Contenful

--- js/content_creator.js
+++ js/content_creator.js
@@ -15,13 +15,16 @@ function createPortfolioItems(data) {
     caption_content.append(caption_icon);
     caption.append(caption_content);
     modal_link.append(caption);
+    modal_link.append(img);
     portfolio_item.append(modal_link);
-    portfolio_item.append(img);
+    
+
+    $("#portfolioItems").append(portfolio_item);
 
   })
 }
 
-function createPortfolioItems(data) {
+function createPortfolioModals(data) {
   data.forEach(function(item) {
     var modal = $('<div class="portfolio-modal modal fade" tabindex="-1" role="dialog" aria-hidden="true"></div>');
     var modal_content = $('<div class="modal-content"></div>');
@@ -40,33 +43,37 @@ function createPortfolioItems(data) {
     var client_link = $('<a></a>'); //TODO surround with strong tags
     var date_list_item = $('<li>Date:&nbsp;</li>');
     var date_strong = $('<strong></strong>');
-    var links_list_item = $('<li>Links&nbsp;</li>');
+    var links_list_item = $('<li>Links:&nbsp;</li>');
     var links_website_strong = $('<strong></strong>');
     var links_sourceCode_strong = $('<strong></strong>');
     var website_link = $('<a>Website</a>');
     var sourceCode_link = $('<a>Source Code</a>');
+    var linksSeparator = $('<span>; </span>');
     var closeButton = $('<button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>');
 
+    modal.attr("id", item.sys.id)
     heading.html(item.fields.title);
-    img.attr("src", "");
-    paragraph.html(details);
+    img.attr("src", item.fields.screenShot.fields.file.url);
+    img.attr("alt", item.fields.screenShot.fields.title);
+    paragraph.html(item.fields.details);
     client_link.attr("href", item.fields.clientLink);
     client_link.html(item.fields.client);
     date_strong.html(item.fields.date);
     website_link.attr("href", item.fields.siteLink);
     sourceCode_link.attr("href", item.fields.sourceCodeLink);
 
-    client_strong.append(client_link);
-    links_website_strong.append(website_link);
-    links_sourceCode_strong.append(sourceCode_link);
+    client_strong.html(client_link);
+    links_website_strong.html(website_link);
+    links_sourceCode_strong.html(sourceCode_link);
 
     client_list_item.append(client_strong);
     date_list_item.append(date_strong);
-    links_list_item.append(links_website_strong, links_sourceCode_strong);
+    links_list_item.append(links_website_strong, linksSeparator, links_sourceCode_strong);
 
     list.append(client_list_item, date_list_item, links_list_item);
 
-    modal_body.append(title, hr, img, paragraph, list, closeButton);
+
+    modal_body.append(heading, hr, img, paragraph, list, closeButton);
     col.append(modal_body);
     row.append(col);
     container.append(row);
@@ -75,6 +82,8 @@ function createPortfolioItems(data) {
     modal_content.append(container);
 
     modal.append(modal_content);
+
+    $("#portfolio-modals").append(modal);
   })
 
 
@@ -94,12 +103,6 @@ function getContentfulData() {
   }).then(function(entries) {
     console.log(entries.items);
     createPortfolioItems(entries.items);
-    entries.items.forEach(function(item) {
-      var fields = Object.keys(item.fields);
-      console.log(fields);
-      fields.forEach(function(field) {
-        console.log(field + ': ' + item.fields[field]);
-      })
-    }, this);
+    createPortfolioModals(entries.items);
   })
 }
